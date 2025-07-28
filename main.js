6ac11de640cef75b2b7945af5d49e231fda055f6
Jouni Kiviperä <jounikivipera@gmail.com>
2025-07-28 16:29:03 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 window.addEventListener('load', function () {
   const game = new Phaser.Game({
     width: 3200,
@@ -15,18 +16,50 @@ window.addEventListener('load', function () {
         debug: true,
       },
     },
-  })
+  });
 
-  game.scene.add('Boot', Boot, true)
-})
+  game.scene.add('Boot', Boot, true);
+  game.scene.add('Menu', Menu);
+  game.scene.add('Game', Game);
+});
 
 class Boot extends Phaser.Scene {
   preload() {
-    this.load.pack('pack', 'assets/asset-pack.json')
-    this.load.image('flame', 'assets/images_environment/flame.png')
+    this.load.pack('pack', 'assets/asset-pack.json');
+    this.load.image('flame', 'assets/images_environment/flame.png');
   }
 
   create() {
-    this.scene.start('Menu')
+    this.scene.start('Menu');
+  }
+}
+
+class Menu extends Phaser.Scene {
+  constructor() {
+    super('Menu');
+  }
+
+  create() {
+    this.add.text(300, 200, 'Aloita peli painamalla ENTER', {
+      font: '32px Arial',
+      fill: '#ff0000',
+    });
+
+    this.input.keyboard.on('keydown-ENTER', () => {
+      this.scene.start('Game');
+    });
+  }
+}
+
+class Game extends Phaser.Scene {
+  constructor() {
+    super('Game');
+  }
+
+  create() {
+    this.add.text(300, 300, 'Peli käynnistyi!', {
+      font: '32px Arial',
+      fill: '#00ff00',
+    });
   }
 }
